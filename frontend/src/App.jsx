e996546220026d6c9c9d5be84d2170d6ea2d42f7
Santiago Cañu <santisv4@gmail.com>
2025-06-14 03:49:06 -0300

    delete header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Header from './components/Header';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
